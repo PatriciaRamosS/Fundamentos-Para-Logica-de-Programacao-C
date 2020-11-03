d2979bcb5e1e872c74f50d38104a87a747c79467
Patrícia Ramos <patricia.silva.ramos@outlook.com>
2020-11-03 15:39:33 -0300

    setlocale()

---
+++ 01 - Tipos de Variáveis/Aula01.c
@@ -0,0 +1,25 @@
+/*Como Usar Acentos*/
+
+
+//Bibliotecas Padrão 
+#include <stdio.h>  //Biblioteca padrão para comandos de Entrada / Saída
+
+#include <stdlib.h> //Este cabeçalho define várias funções de propósito geral, 
+                    //incluindo gerenciamento de memória dinâmica, geração de números aleatórios, 
+                    //comunicação com o ambiente, aritmética de inteiros, pesquisa, classificação e conversão.
+
+#include<locale.h> //necessário para usar setlocale
+
+void main() //função principal
+
+{//Início do corpo da função principal
+
+    // Utilizamos a função setlocale() para fazer a adaptação do programa ao idioma desejado.
+    setlocale(LC_ALL, "Portuguese");
+
+    //Resultado
+    printf("Olá"); 
+
+    return 0; //valor de retorno das função main
+
+ } //Fim do corpo da função principal e fim do programa
